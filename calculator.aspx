77bfedc8a92e6987d8caafd8ca334f6a52cf93d1
Pooja Patel <poojapatel15115@gmail.com>
2024-12-24 11:45:14 +0530

    calculator

---
+++ calculator.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calculator.aspx.cs" Inherits="WebApplication10.calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="tx" runat="server" Height="69px" Width="452px"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <asp:Button ID="seven" runat="server" Height="80px" Text="7" Width="80px" OnClick="seven_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="eight" runat="server" Height="80px" Text="8" Width="80px" OnClick="eight_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="nine" runat="server" Height="80px" Text="9" Width="80px" OnClick="nine_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="plus" runat="server" Height="80px" Text="+" Width="80px" OnClick="plus_Click" />
+        <p>
+            <asp:Button ID="four" runat="server" Text="4" Width="80px" Height="80px" OnClick="four_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="five" runat="server" Text="5" Height="80px" OnClick="five_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="six" runat="server" Text="6" Height="80px" OnClick="six_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="minus" runat="server" Text="-" Height="80px" OnClick="minus_Click" Width="80px" />
+        </p>
+        <p>
+            <asp:Button ID="one" runat="server" Text="1" Height="80px" OnClick="one_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="two" runat="server" Text="2" Height="80px" OnClick="two_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+            <asp:Button ID="three" runat="server" Text="3" Height="80px" OnClick="three_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="multipy" runat="server" OnClick="Button12_Click" Text="*" Height="80px" Width="80px" />
+        </p>
+        <p>
+            <asp:Button ID="clear" runat="server" Text="Clear" Height="80px" OnClick="clear_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+            <asp:Button ID="zero" runat="server" Text="0" Height="80px" OnClick="zero_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+            <asp:Button ID="division" runat="server" Text="/" Height="80px" OnClick="division_Click" Width="80px" />
+&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+            <asp:Button ID="answer" runat="server" Text="=" Height="80px" OnClick="answer_Click" Width="80px" />
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ calculator.aspx.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication10
+{
+    public partial class calculator : System.Web.UI.Page
+    {
+        int num1;
+
+        string op = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void zero_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "0";
+            }
+            else
+            {
+                tx.Text = tx.Text + "0";
+            }
+        }
+
+        protected void one_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "1";
+            }
+            else
+            {
+                tx.Text = tx.Text + "1";
+            }
+        }
+
+        protected void two_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "2";
+            }
+            else
+            {
+                tx.Text = tx.Text + "2";
+            }
+        }
+
+        protected void three_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "3";
+            }
+            else
+            {
+                tx.Text = tx.Text + "3";
+            }
+        }
+
+        protected void four_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "4";
+            }
+            else
+            {
+                tx.Text = tx.Text + "4";
+            }
+        }
+
+        protected void five_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "5";
+            }
+            else
+            {
+                tx.Text = tx.Text + "5";
+            }
+
+        }
+
+        protected void six_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "6";
+            }
+            else
+            {
+                tx.Text = tx.Text + "6";
+            }
+        }
+
+        protected void seven_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "7";
+            }
+            else
+            {
+                tx.Text = tx.Text + "7";
+            }
+        }
+
+        protected void eight_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "8";
+            }
+            else
+            {
+                tx.Text = tx.Text + "8";
+            }
+        }
+
+        protected void nine_Click(object sender, EventArgs e)
+        {
+            if (tx.Text == "0" && tx.Text != null)
+            {
+                tx.Text = "9";
+            }
+            else
+            {
+                tx.Text = tx.Text + "9";
+            }
+        }
+
+        protected void clear_Click(object sender, EventArgs e)
+        {
+            tx.Text = "";
+
+        }
+
+        protected void plus_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(tx.Text);
+            tx.Text = "0";
+            op = "+";
+        }
+
+        protected void minus_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(tx.Text);
+            tx.Text = "0";
+            op = "-";
+        }
+
+        protected void division_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(tx.Text);
+            tx.Text = "0";
+            op = "/";
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(tx.Text);
+            tx.Text = "0";
+            op = "*";
+        }
+
+        protected void answer_Click(object sender, EventArgs e)
+        {
+            int num2;
+            int result;
+            num2 = Convert.ToInt32(tx.Text);
+            if (op == "+")
+            {
+                result = num1 + num2;
+                tx.Text = result.ToString(); num1 = result;
+            }
+            else if (op == "-")
+            {
+                result = num1 - num2; tx.Text = result.ToString();
+                num1 = result;
+            }
+            else if (op == "*")
+            {
+                result = num1 * num2;
+                tx.Text = result.ToString();
+                num1 = result;
+            }
+            else if (op == "/")
+            {
+                if (num2 == 0)
+                {
+                    tx.Text = "Cannot divide by zero";
+                }
+                else
+                {
+                    result = num1 / num2;
+                    tx.Text = result.ToString();
+                    num1 = result;
+                }
+            }
+        }
+    }
+}
